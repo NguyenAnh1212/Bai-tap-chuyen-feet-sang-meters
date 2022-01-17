b2d5846ca4b1e9693f3798554a2a3c1518542021
unknown <anhnt.tb12@gmail.com>
2022-01-17 14:44:04 +0700

    hoan thien chuyen doi

--- js.js
+++ js.js
@@ -1,8 +1,20 @@
 function footToMeter(foot){
-    let result=0.305*foot;
-    return result
+    let meter =0.305*foot;
+    return meter;
     }
 function meterToFoot(meter){
-    let result=3.279*meter;
-    return result
+    let foot =3.279*meter;
+    return foot;
+}
+function meterConverter() {
+    let a = Number(document.getElementById("inputFoot").value);
+    let result = footToMeter(a);
+    document.getElementById("outputMeter").innerText=result;
+    
+}
+function footConverter() {
+    let a = Number(document.getElementById("inputMeter").value);
+    let result = meterToFoot(a);
+    document.getElementById("outputFoot").innerText=result;
+
 }
